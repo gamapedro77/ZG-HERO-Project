30dedbff659e546dc598d3b4925ebd516a08029d
Pedro Gama <pedrolucasdgm@gmail.com>
2022-07-08 09:00:55 -0300

    adicionando documentacao da API de cadastro

--- MS-Vagas/grails-app/controllers/ms/vagas/ApplicationController.groovy
+++ MS-Vagas/grails-app/controllers/ms/vagas/ApplicationController.groovy
@@ -2,7 +2,10 @@ package ms.vagas
 
 import grails.core.GrailsApplication
 import grails.plugins.*
+import io.swagger.annotations.Api
 
+
+@Api(value = "Controller default", tags="Ignore")
 class ApplicationController implements PluginManagerAware {
 
     GrailsApplication grailsApplication
