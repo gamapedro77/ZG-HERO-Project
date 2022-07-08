0850f383643c96e9cd08fb5461468080fb1dba2c
Pedro Gama <pedrolucasdgm@gmail.com>
2022-07-08 10:52:54 -0300

    criacao de rotas e modulos para perfil candidato e perfil empresa

---
+++ front-linketinder/src/app/candidato/candidato.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CandidatoRoutingModule } from './candidato-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    CandidatoRoutingModule
+  ]
+})
+export class CandidatoModule { }
--- front-linketinder/src/app/login/home/home.component.ts
+++ front-linketinder/src/app/login/home/home.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import { CandidatoService } from 'src/app/cadastro/services/candidato.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
+  form: FormGroup;
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private service: CandidatoService,
+    private router: Router
+  ) {
+    this.form = this.formBuilder.group({
+      email: [null],
+      senha: [null],
+    });
+  }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  onSubmit() {
+    console.log('doideira maluco');
   }
 
+  onCadastro() {
+    console.log('maconha');
+    //this.router.navigate(['/cadastro']);
+  }
 }
