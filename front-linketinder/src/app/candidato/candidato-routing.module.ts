c131d27e504cf38a3377ede6d9f310e9f768d1ef
Pedro Gama <pedrolucasdgm@gmail.com>
2022-07-08 12:57:23 -0300

    telas de empresa e candidato

--- front-linketinder/src/app/candidato/candidato-routing.module.ts
+++ front-linketinder/src/app/candidato/candidato-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PerfilComponent } from './perfil/perfil.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'perfil', component: PerfilComponent }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class CandidatoRoutingModule { }
+export class CandidatoRoutingModule {}
--- front-linketinder/src/app/empresa/empresa.module.ts
+++ front-linketinder/src/app/empresa/empresa.module.ts
@@ -2,13 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { EmpresaRoutingModule } from './empresa-routing.module';
-
-
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
 @NgModule({
-  declarations: [],
+  declarations: [DashboardComponent],
   imports: [
     CommonModule,
-    EmpresaRoutingModule
-  ]
+    EmpresaRoutingModule,
+    MatSidenavModule,
+    MatButtonModule,
+    MatIconModule,
+    MatDividerModule,
+  ],
 })
-export class EmpresaModule { }
+export class EmpresaModule {}
